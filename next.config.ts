311a45187d7ba1e715a28b9c9b7a8e2dd20e1674
Arslan ALAN <alan.arslan@gmail.com>
2025-04-29 16:23:15 +0300

    Config files updated for Azure Deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   images: {
     domains: ['lifexstorage.blob.core.windows.net'],
   },
