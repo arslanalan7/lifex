1be11d26108a766864a5e74823c4b7fbe1d4cdac
Arslan ALAN <alan.arslan@gmail.com>
2025-05-03 13:00:53 +0300

    updated next.config.ts and workflow file to fix url error

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
   env: {
-    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'lifex-app-gzbug7e6g4gxdmdz.westeurope-01.azurewebsites.net',
   },
   images: {
     domains: ['lifexstorage.blob.core.windows.net'],
