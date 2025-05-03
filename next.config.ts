53eb82aa9cac10aad5b2875c4b9df2f22f5b50e9
Arslan ALAN <alan.arslan@gmail.com>
2025-05-03 12:47:54 +0300

    updated next.config.ts to fix url error

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  },
   images: {
     domains: ['lifexstorage.blob.core.windows.net'],
   },
